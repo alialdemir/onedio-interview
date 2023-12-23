5f478e3b12f87ffcc0025960c795c0448414bfcd
Ali <aldemirali93@gmail.com>
2023-12-23 16:57:48 +0300

    iki tarih arasındaki maçlar getirildi

--- src/controllers/footballMatchController.js
+++ src/controllers/footballMatchController.js
@@ -3,9 +3,14 @@ const { getFootballMatchByPaging } = require('../services/footballMatchService')
 
 const getFootballMatches = async (ctx) => {
   const page = parseInt(ctx.query.page) || 1;
-  const limit = parseInt(ctx.query.limit) || 10;
+  const limit = parseInt(ctx.query.limit) || 15;
 
-  const footballMatches = await getFootballMatchByPaging(page, limit)
+  const division = ctx.query.Division || 'All';
+
+  const startYear = new Date(ctx.query.startYear);
+  const endYear = new Date(ctx.query.endYear);
+
+  const footballMatches = await getFootballMatchByPaging(page, limit, startYear, endYear, division)
 
   ctx.body = {
     page,
--- src/services/footballMatchService.js
+++ src/services/footballMatchService.js
@@ -1,26 +1,46 @@
-const { db, FootballMatchSchema, } = require('../database/mongoDBRepository');
+const { db } = require('../database/mongoDBRepository');
+const FootballMatchSchema = require('../database/schemas/footballMatchSchema');
 const { logError, logSuccess, delay } = require('../utils');
 const { getFromCache, setToCache } = require('./cacheService');
 const mongoose = require('mongoose');
 
+const leagues = {
+    'Premier League': 'E0',
+    'Bundesliga': 'D1'
+}
 
-const getFootballMatchByPaging = async (page, limit) => {
+const getFootballMatchByPaging = async (page, limit, startDate, endDate, division) => {
     // const cacheKey = `footballMatch:${page}`;
     const cachedData = undefined// await getFromCache(cacheKey);
     if (cachedData) {
         return cachedData
     }
+
+    const query = {
+        Date: {
+            $gte: startDate,
+            $lte: endDate,
+        },
+        Div: 'E0'
+    }
+
+    if (division !== 'All') {
+        query.Div = leagues[division]
+    }
+
+    console.log('div', query)
     const skip = (page - 1) * limit;
 
     const FootballMatch = mongoose.model('FootballMatch', FootballMatchSchema);
-    const footballMatches = await FootballMatch.find()
+    const footballMatches = await FootballMatch
+        .find(query)
         .select('-_id Date HomeTeam AwayTeam FTHG FTAG Referee')
         .skip(skip)
         .limit(limit)
         .exec()
 
-    const totalDocuments = await FootballMatch.countDocuments();
-
+    const totalDocuments = await FootballMatch.countDocuments(query);
+    // console.log(footballMatches)
     // await setToCache(cacheKey, JSON.stringify(footballMatches));
 
     return {
@@ -33,6 +53,25 @@ const addFootballMatch = (footballMatch) => {
     try {
         // Insert data into MongoDB
         const FootballMatch = db.model('FootballMatch', FootballMatchSchema);
+        footballMatch = footballMatch.map(match => {
+            if (match.Date) {
+                try {
+                    let [day, month, year] = match.Date.split('/');
+
+                    // These date formats are special case for 20/08/17
+                    if (year.toString().length === 2) {
+                        year += 2000
+                    }
+
+                    const formattedDate = `${year}-${month}-${day}`;
+
+                    match.Date = new Date(formattedDate)
+                } catch (error) {
+                }
+            }
+
+            return match
+        })
 
         // Insert multiple records into the MongoDB collection
         FootballMatch.insertMany(footballMatch);
