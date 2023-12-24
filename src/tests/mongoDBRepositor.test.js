6e0c828aedcee15ee6885216861fb1546e966842
Ali <aldemirali93@gmail.com>
2023-12-24 11:01:32 +0300

    mongo repository test

---
+++ src/tests/mongoDBRepositor.test.js
@@ -0,0 +1,17 @@
+const { expect } = require('chai');
+const mongoose = require('mongoose');
+const { db } = require('../database/mongoDBRepository');
+
+describe('MongoDB Connection', () => {
+    it('should connect to MongoDB successfully', (done) => {
+        expect(db.readyState).to.equal(1);// 1: connected
+
+        done();
+    });
+
+    after((done) => {
+        mongoose.disconnect();
+
+        done();
+    });
+});
