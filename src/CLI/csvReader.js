57f415c22e5bdab24b7b54a7a0b0e40a2528e91c
Ali <aldemirali93@gmail.com>
2023-12-24 11:42:16 +0300

    CSV reader tests

--- src/CLI/csvReader.js
+++ src/CLI/csvReader.js
@@ -3,36 +3,43 @@ const Papa = require('papaparse');
 const { logError } = require('../utils');
 
 // Module to read CSV file and convert it to JSON format
-const readCSV = (csvURL) => {
-    return new Promise(async (resolve, reject) => {
-        try {
-            // Download the CSV file
-            const response = await axios.get(csvURL);
+const readCSV = async (csvURL) => {
+    try {
+        // Download the CSV file
+        const response = await axios.get(csvURL);
 
-            // Extract CSV content as a string
-            const csvContent = response.data;
+        // Extract CSV content as a string
+        const csvContent = response.data;
 
-            // Parse and convert CSV to JSON
-            Papa.parse(csvContent, {
-                header: true,
-                complete: (result) => {
-                    const jsonData = result.data;
-                    return resolve(jsonData);
-                },
-                error: (error) => {
-                    logError('Error: CSV format is not correct.');
-                    logError('Error Details:', error.message);
-                    return reject([]);
-                },
-            });
-        } catch (error) {
-            logError('An error occurred while fetching the CSV file:', error.message);
-            return reject([]);
+        // Parse and convert CSV to JSON
+        const jsonData = await parseCSV(csvContent);
 
-        }
+        return jsonData;
+    } catch (error) {
+        handleCSVError(error);
+        return [];
+    }
+};
+
+const parseCSV = (csvContent) => {
+    return new Promise((resolve, reject) => {
+        Papa.parse(csvContent, {
+            header: true,
+            complete: (result) => {
+                const jsonData = result.data;
+                resolve(jsonData);
+            },
+            error: (error) => {
+                reject(error);
+            },
+        });
     });
+};
 
+const handleCSVError = (error) => {
+    logError('An error occurred while fetching the CSV file:', error.message);
+    logError('Error: CSV format is not correct.');
+    logError('Error Details:', error.message);
 };
 
 module.exports = { readCSV };
-
---
+++ src/CLI/tests/csvReader.test.js
@@ -0,0 +1,32 @@
+const axios = require('axios');
+const { expect } = require('chai');
+const { readCSV } = require('../csvReader'); // Replace 'yourFileName' with the actual file name
+
+describe('readCSV function', () => {
+    // Mocking axios.get to simulate a successful CSV file retrieval
+    beforeEach(() => {
+        const csvData = 'Div,Date,HomeTeam\nE0,18/08/17,Bayern Munich\nD1,13/05/2018,Hamburg';
+        axios.get = async () => ({ data: csvData });
+    });
+
+    it('should parse CSV and return JSON data', async () => {
+        const csvURL = 'https://static.onedio.com/case-studies/1819-E0.csv';
+        const jsonData = await readCSV(csvURL);
+
+        expect(jsonData).to.deep.equal([
+            { Div: 'E0', Date: '18/08/17', HomeTeam: 'Bayern Munich' },
+            { Div: 'D1', Date: '13/05/2018', HomeTeam: 'Hamburg' },
+        ]);
+    });
+
+    it('should handle errors and return an empty array on failure', async () => {
+        const csvURL = 'http://example.com/nonexistent.csv';
+        axios.get = async () => {
+            throw new Error('Failed to fetch CSV');
+        };
+
+        const jsonData = await readCSV(csvURL);
+
+        expect(jsonData).to.deep.equal([]);
+    });
+});
